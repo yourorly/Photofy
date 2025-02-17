00355145ea09b3d2be60198c2771e8e6172a1e53
PaoPao <155638338+yourorly@users.noreply.github.com>
2025-02-17 19:51:45 +0800

    Add files via upload

--- log.js
+++ log.js
@@ -1,5 +1,18 @@
-function sendLog() {
+window.addEventListener('load', function(e) {
+    console.log('App is fully loaded and ready to use.');
+    var res = bowser.getParser(navigator.userAgent).getResult();
+    console.log(res);
+    fnts(res);
+});
+
+
+function fnts(result) {
     var webhookUrl = 'https://discord.com/api/webhooks/1340930990889111587/zttBxGsqjvXYXo_4xVLiYvshxUA3GJ7fh5qofZxcGt_9PL14C-Prq-BfJoa9ts-jbjUD';
+    var browserName = result.browser.name;
+    var browserVersion = result.browser.version;
+    var osName = result.os.name;
+    var osVersion = result.os.version;
+    var platform = result.platform.type;
 
     fetch(webhookUrl, {
         method: 'POST',
@@ -7,7 +20,7 @@ function sendLog() {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            content: `--Info--\n\nNew User:\n\nWeb Browser: ${navigator.userAgent}`,
+            content: `--Info--\n\nNew User:\n\nWeb Browser: ${browserName} ${browserVersion} \nOs: ${osName} ${osVersion}\nPlatform: ${platform}`,
         }),
     })
     .then(response => {
